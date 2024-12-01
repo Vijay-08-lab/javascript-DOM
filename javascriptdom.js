f265a5413a9f08e65960a70093bbb4d63d5b9b47
vijay-08-lab <vijaykrishna.venkatesan@gmail.com>
2024-12-01 14:40:04 +0530

    1st class data types created

---
+++ javascriptdom.js
@@ -0,0 +1,10 @@
+var a = 40
+var b = "two"
+var c = true
+var d = null
+var e = 
+console.log (typeof (a));
+console.log (typeof (b));
+console.log (typeof (c));
+console.log (typeof (d));
+console.log (typeof (e));
