21c606bc9b83f6f89bdcb070cfd91a2bd0c9a5f0
vijay-08-lab <vijaykrishna.venkatesan@gmail.com>
2024-12-02 10:26:39 +0530

    yesterday's class

--- javascriptdom.js
+++ javascriptdom.js
@@ -1,3 +1,4 @@
+/*
 // javascript Data types//
 var a = 40
 var b = "two"
@@ -9,3 +10,123 @@ console.log (typeof (b));
 console.log (typeof (c));
 console.log (typeof (d));
 console.log (typeof (e));
+// Javascript functions //
+
+//Task function 1 //
+ /* add the 2 values and get the results */
+/*
+ function addtion () {
+    var a = 30
+    var b = 20
+    console.log (a+b)
+}
+addtion()
+
+// Task functions 2//
+/* to create a function for three mobile companies*/
+/*
+function Apple ()
+{
+    console.log ("hai i am iphone")
+}
+Apple()
+function opp () {
+    console.log("hai i am oppo")
+}
+function redmi (){
+    console.log("hai this is redmi")
+}
+redmi()
+opp()
+*/
+// Task function 3 //
+/* create variable  callled  factor,fplayer,fmovie. create a function called favourite(). everytime  you call this function.*/
+/*var factor ="Kamal"
+var fplayer ="Dhoni"
+var fmovie ="Anbe sivam"
+function favourite()
+{
+    console.log ("Favourite Actor :" +factor)
+    console.log ("Favourite Player :" +fplayer)
+    console.log ("Favourite Movie :" +fmovie)
+}
+favourite()
+*/
+// Parameter of javascript //
+// Task Parameters in javascript //
+/*
+function area (Length,Breadth)
+{
+    console.log ("Lenght is :" +Length ,"Breadth is :" + Breadth)
+    console.log ("Area is :" + Length * Breadth)
+}
+area(10,10)
+*/
+//return type in Javascript //
+/*
+function add(a,b)
+{
+    return (a*b)
+    }
+ var total = add(10,20)
+ console.log(total)
+ */
+// if else statement in Javascript //
+ // Task if else 1 //
+// Did you finish your homework? // 
+ let homework = true
+ if (true)
+ {
+    console.log("Great job!")
+}
+else {
+    console.log ("Finish your homework before playing")
+}
+// Task if else 2 //
+// Are there any cookies left? //
+var cookies = true
+if (true)
+{
+    console.log("Would you like a cookie?")
+}
+else {
+    console.log("Time to bake more cookies")
+}
+// Task if else 3 //
+// Did you brush your teeth? //
+var brush = true
+if (true)
+{
+    console.log("Your teeth are clean")
+}
+else {
+    console.log("Go brush your teeth now")
+}
+// Task if else 4 //
+// Is it your birthday today? //
+var Birthday = true
+if (true)
+    {
+        console.log("Happy Birthday Enjoy your special day!")
+    }
+    else 
+    {
+        console.log("Have a great day")
+    }
+    // Logical And OR NOT in javascript //
+    // Task logical javascript 1 //
+    //what is the season of the year?//
+    
+    var season = "spring"
+if (season == "spring") {
+    console.log("Enjoy the Blooming folwer!")
+}
+if (season == "summer") {
+    console.log("Have fun in the sun.")
+}
+if (season == "autumn") {
+    console.log("Admire the colorful leaves.")
+}
+if (season == "winter") {
+    console.log("Bundel up and stay warm")
+}
